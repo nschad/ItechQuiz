fae445da56a452a23df26ee700799f7d53a6fb0a
Niclas Schad <nschad@havi.de>
2019-07-02 13:25:36 +0200

    Init

---
+++ quiz/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import User
+import datetime
+
+class Answer(models.Model):
+    answer = models.TextField(max_length=512)
+    is_correct = models.BooleanField(default=False)
+
+
+class Quiz(models.Model):
+    question = models.TextField(max_length=512)
+    answers = models.ForeignKey(Answer, on_delete=models.CASCADE)
+
+
+class HighScore(models.Model):
+    score = models.IntegerField()
+    player = models.ForeignKey(User, on_delete=models.CASCADE)
+    archived_at = models.DateTimeField(default=datetime.datetime.now(), blank=False)
