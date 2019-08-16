a7b753a5c742d3fd38514cf45d1755e7671cb166
ShuzZzle <niclas.schad@gmail.com>
2019-08-16 17:33:45 +0200

    Gedöns

--- quiz/forms.py
+++ quiz/forms.py
@@ -9,27 +9,3 @@ class BootstrapAuthenticationForm(AuthenticationForm):
     password = forms.CharField(widget=forms.PasswordInput(
         attrs={'id': 'inputPassword', 'type': 'password', 'placeholder': 'Password', 'class': 'form-control'}))
 
-
-class MySuperCustomCheckboxField(forms.CheckboxInput):
-
-    def __init__(self, question_name=""):
-        super().__init__()
-        self.question_name = question_name
-
-
-class PlayForm(forms.Form):
-    def __init__(self, formdata: dict = None):
-        super().__init__()
-
-        if formdata is None:
-            raise ValueError("Passing no form data is kinda Shitty mate.")
-
-        questions = formdata["answers"]
-
-        for question in questions:
-            original_question_text = question
-            question_id = question.replace(" ", "_").strip()
-            self.fields[question_id] = forms.CharField(widget=MySuperCustomCheckboxField(
-                question_name=original_question_text))
-
-
--- quiz/views.py
+++ quiz/views.py
@@ -1,12 +1,13 @@
 from quiz.models import *
 from django.http import HttpResponse
 from django.views.generic import View
-from quiz.forms import PlayForm
 from django.shortcuts import redirect, render
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
-from quiz.models import Quiz
+from quiz.models import Quiz, Options
 import random
+from quiz.serializers import QuizSerializer
+
 
 @method_decorator(login_required(login_url='/login/'), name='dispatch')
 class PlayView(View):
@@ -14,26 +15,34 @@ class PlayView(View):
     def __init__(self):
         super().__init__()
 
-    def get(self, request, *args, **kwargs):
-        stockthingy = {
-            'id': 1,
-            'question_name': 'Bist du behindert?',
-            'answers': ['Ja', 'Nein', 'Selber behindert!'],
-            'correct_answer_id': 1
-        }
-        # Fetches Random Record.
-        quiz = Quiz.objects.all().prefetch_related('answers')
-        blub = random.choice(quiz)
-        from quiz.serializers import QuizSerializer
+    @staticmethod
+    def get_random_question() -> dict:
+        data = {}
+        if Quiz.objects.all().count() == 0:
+            return None
 
-        data = QuizSerializer.QuizSerializer(blub).data
-        print(data)
-        answers = blub.answers.all()
-        print(answers)
-        stockthingy['question_name'] = blub.question
-        stockthingy['answers'] = blub.answers
-        form = PlayForm(formdata=stockthingy)
-        return render(request, 'play.html', {'question_name': blub.question, 'playform': form})
+        quiz = Quiz.objects.all().prefetch_related('answers')
+        question = random.choice(quiz)
+
+        question_data = QuizSerializer.QuizSerializer(question).data
+        answers = question_data['answers']
+
+        data['question_name'] = question_data['question']
+
+        answers = Options.objects.filter(id__in=answers)
+        data['answers'] = []
+        for answer in answers:
+            data['answers'].append({'answer': answer.option, 'answer_id': answer.id})
+
+        data['question_key'] = question.id
+
+        return data
+
+    def get(self, request, *args, **kwargs):
+        data = self.get_random_question()
+
+        # POW!
+        return render(request, 'play.html', {'data': data})
 
     def post(self, request, *args, **kwargs):
         """
@@ -45,6 +54,6 @@ class PlayView(View):
         """
         data: dict = request.POST.dict()
         data.pop('csrfmiddlewaretoken', None)
-        print(data)
+        answer_ids = request.POST.getlist('selected_answers[]')
 
-        return HttpResponse("DAFUQ")
+        return redirect('play')
