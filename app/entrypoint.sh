fcc0610df72d058aa9785783283185967add65d2
ShuzZzle <shuzzzle@forwarduntodawn.shuzzzle.com>
2019-09-12 16:03:35 +0200

    Added Loading of Le Questions.

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -4,5 +4,6 @@ python manage.py flush --no-input
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --no-input --clear
+python manage.py load_questions /usr/src/app/ItechQuiz/questions.json
 
 exec "$@"
